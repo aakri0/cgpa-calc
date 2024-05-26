62832cd44deb3054204d393c7089c820f2e66f11
aakri0 <139072694+aakri0@users.noreply.github.com>
2024-05-27 00:32:42 +0530

    updated src and completed entire code logic

--- src.cpp
+++ src.cpp
@@ -7,8 +7,7 @@ Write a basic math eqn to calc CG
 Take the avg of grades obtained throughout the semester
 */
 
-#include<iostream>
-#include<map>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -18,7 +17,7 @@ int main() {
     cout << "Enter the number of subjects: ";
     int no_of_subs;
     cin >> no_of_subs;
-    cout << "Enter the number of subjects and the credits per subject: (course_name credits)" << endl;
+    cout << "Enter the name of subjects and the credits per subject: (course_name credits)" << endl;
     map<string, int> m;
     int total_credits = 0;
     for(int i = 0; i < no_of_subs; ++i){
@@ -30,16 +29,33 @@ int main() {
     }
 
     //Logic for marks obtained in each subject
-    vector<pair<string, int>> avg_marks(no_of_subs);
-    cout << "Enter the subject name and marks obtained in CIE-1, CIE-2 and CIE-3:" << endl;
+    vector< pair<string, int> > avg_marks(no_of_subs);
+    cout << "Enter the subject name and best marks obtained in any two out of CIE-1, CIE-2 and CIE-3:" << endl;
     for(int i = 0; i < no_of_subs; ++i){
         string sub_name; cin >> sub_name;
-        int a, b, c; cin >> a >> b >> c;
-        int avg = (a+b+c)/3;
+        int a, b; cin >> a >> b;
+        int avg = (a+b);
         avg_marks[i] = {sub_name, avg};
     }
 
-    //Formula for CGPA
-    
+    //Storing grade point for each subject
+    vector<int> gp;
+    for(auto &it : m){
+        for(int i = 0; i < no_of_subs; ++i){
+            if(it.first == avg_marks[i].first){
+                if(avg_marks[i].second >= 90) gp.push_back(10*it.second);
+                else if(avg_marks[i].second >= 80) gp.push_back(9*it.second);
+                else if(avg_marks[i].second >= 70) gp.push_back(8*it.second);
+                else if(avg_marks[i].second >= 60) gp.push_back(7*it.second);
+                else if(avg_marks[i].second >= 50) gp.push_back(6*it.second);
+                else if(avg_marks[i].second >= 40) gp.push_back(5*it.second);
+                else gp.push_back(0);
+            }
+        }
+    }
+
+    int sum_gp = accumulate(gp.begin(), gp.end(), 0);
+    float CG = sum_gp/total_credits;
+    cout << "Your CGPA is: " << (float)CG << endl;
 
 }
