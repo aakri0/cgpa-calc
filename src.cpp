681b592474a86c48fe5480cefe4711dbf5a4bc96
aakri0 <139072694+aakri0@users.noreply.github.com>
2024-05-26 17:53:48 +0530

    added src file

---
+++ src.cpp
@@ -0,0 +1,40 @@
+//Basic Logic
+/*
+Take input from user about subjects
+Get the credits for each subject and the grades obtained
+
+Write a basic math eqn to calc CG
+Take the avg of grades obtained throughout the semester
+*/
+
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main() {
+    cout << "Calculate your CGPA after internalization!" << endl;
+    
+    //Take input abt sub and credits
+    cout << "Enter the number of subjects: ";
+    int no_of_subs;
+    cin >> no_of_subs;
+    cout << "Enter the number of subjects and the credits per subject: (course_name credits)" << endl;
+    map<string, int> m;
+    int total_credits = 0;
+    for(int i = 0; i < no_of_subs; ++i){
+        string x; int y;
+        //x is course_name and y is credits
+        cin >> x >> y;
+        total_credits += y;
+    }
+
+    //Obtain total credits of the particular semester
+    int total_credits = 0;
+    for(auto &it : m){
+        total_credits += it.second;
+    }
+
+    //Logic for marks obtained in each subject
+    
+
+}
